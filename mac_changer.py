a60f637cdf4a99af31715405399dbc7e497a51e2
Jay-Goss <42222447+Jay-Goss@users.noreply.github.com>
2019-03-28 23:23:03 -0400

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -6,9 +6,15 @@ interface = input("interface > ")
 new_mac = input("new MAC > ")
 print('[+] changing MAC address for ' + interface + ' to the new address ' + new_mac)
 
-subprocess.call('ifconfig ' + interface + ' down', shell=True)
-subprocess.call('ifconfig ' + interface + ' hw ether ' + new_mac, shell=True)
-subprocess.call('ifconfig ' + interface + ' up', shell=True)
+subprocess.call(['ifconfig', interface, 'down'])
+subprocess.call(['ifconfig', interface, 'hw', 'ether', new_mac])
+subprocess.call(['ifconfig', interface, 'up'])
+
+# fixes the vulnerability by allowing no additional commands
+
+# subprocess.call('ifconfig ' + interface + ' down', shell=True)
+# subprocess.call('ifconfig ' + interface + ' hw ether ' + new_mac, shell=True)
+# subprocess.call('ifconfig ' + interface + ' up', shell=True)
 
 # this format of subprocess is vulnerable to command injections
 # interface = eth0;ls; will add in an ls command
